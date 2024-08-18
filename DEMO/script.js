c3a43c7890228cd83fbd213640a2194978070ed2
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-08-18 07:41:00 +0530

    Rearranged

---
+++ DEMO/script.js
@@ -0,0 +1,19 @@
+console.log("Hello world")
+let a=19;
+console.log(typeof(a))
+console.log(a+2)
+let name="hello"
+console.log(typeof(name))
+console.log(name,a)
+window.alert("alert")
+let age=window.prompt("enter your age")
+console.log(`your age is ${age}`)
+let n=10;
+n= document.getElementById("num")
+function hello(){
+    console.log("This is a function")
+    for(let i=0;i<n;i++)
+    {
+        console.log("Hello world")
+    }
+}
